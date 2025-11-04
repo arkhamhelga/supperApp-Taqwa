30caa450673e7a62795b040c8385fb7f06707b11
arkhamhelga <arkham.helga.taulana>
2025-11-04 11:01:46 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:bitaqwa/pages/home_pages.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        // '/' nama route dari halaman HomePage()
+        // 'zakat' nama route dari halaman zakat
+        '/': (context) => HomePage(),
+      },
+    );
+  }
+}
